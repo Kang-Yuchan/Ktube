45b3d321a0d3ce4c53986f62c2b113edbcd1e64d
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-10 15:06:27 +0900

    Express core middlewares

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -9,6 +13,14 @@ const handleListening = () => console.log(`http://localhost:${PORT}에서 실행
 const handleHome = (req, res) => res.send('Hello from Home');
 
 const handleProfile = (req, res) => res.send('Hello from Profile');
+
+//midlleware
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+//
 
 app.get('/', handleHome);
 app.get('/profile', handleProfile);
