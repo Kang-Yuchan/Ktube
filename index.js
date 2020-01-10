f27d39d67f8ace1a47f2eb542dc64b5649384024
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-10 11:56:54 +0900

    Installing  babel and nodemon for ES6

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 const PORT = 5000;
 
-const handleListening = () => {
-	console.log(`http://localhost:${PORT}에서 실행중`);
-};
+const handleListening = () => console.log(`http://localhost:${PORT}에서 실행중`);
 
-const handleHome = (req, res) => {
-	res.send('Hello from home');
-};
+const handleHome = (req, res) => res.send('Hello from Home');
 
-const handleProfile = (req, res) => {
-	res.send('Hello from Profile');
-};
+const handleProfile = (req, res) => res.send('Hello from Profile');
 
 app.get('/', handleHome);
 app.get('/profile', handleProfile);
