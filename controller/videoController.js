9af5e1f63ef53a605ddc5b2104d85e4ba909391a
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-12 06:55:26 +0900

    Layout with Pug

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,7 +1,7 @@
 export const home = (req, res) => res.render('Home');
-export const search = (req, res) => res.send('Search');
-export const videos = (req, res) => res.send('Videos');
-export const upload = (req, res) => res.send('upload');
-export const videoDetail = (req, res) => res.send('VideoDetail');
-export const editVideo = (req, res) => res.send('Edit Video');
-export const deleteVideo = (req, res) => res.send('Delete Video');
+export const search = (req, res) => res.render('Search');
+export const videos = (req, res) => res.render('Videos');
+export const upload = (req, res) => res.render('upload');
+export const videoDetail = (req, res) => res.render('VideoDetail');
+export const editVideo = (req, res) => res.render('Edit Video');
+export const deleteVideo = (req, res) => res.render('Delete Video');
