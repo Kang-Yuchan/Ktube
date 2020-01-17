4c3a2ec300005583cc83dfe3c710c896d7566b67
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-17 14:47:02 +0900

    Fix bug

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,10 +1,10 @@
-export const home = (req, res) => res.render('Home', { pageTitle: 'Home' });
+export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
 export const search = (req, res) => {
 	const { query: { term: searchingBy } } = req;
-	res.render('Search', { pageTitle: 'Search', searchingBy });
+	res.render('search', { pageTitle: 'Search', searchingBy });
 };
-export const videos = (req, res) => res.render('Videos', { pageTitle: 'Videos' });
+export const videos = (req, res) => res.render('videos', { pageTitle: 'Videos' });
 export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
-export const videoDetail = (req, res) => res.render('VideoDetail', { pageTitle: 'Video Detail' });
-export const editVideo = (req, res) => res.render('Edit Video', { pageTitle: 'Edit Video' });
-export const deleteVideo = (req, res) => res.render('Delete Video', { pageTitle: 'Delete Video' });
+export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
+export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'Edit Video' });
+export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video' });
